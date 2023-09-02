e6222ef3afa4f13ed3ae322d57a3d70a9710e0d6
HP <reshmaksriet@gmail.com>
2023-09-02 15:31:51 +0530

    Initial commit

---
+++ Phase3Section2.7/Phase3Section2.7/Controllers/HomeController.cs
@@ -0,0 +1,93 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.EnterpriseServices;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using System.Web;
+/*using Microsoft.AspNetCore.Mvc;
+using Phase3Section2._7.Models;*/
+
+namespace Phase3Section2._7.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Privacy()
+        {
+            return View();
+        }
+/*
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public ActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        private ActionResult View(ErrorViewModel errorViewModel)
+        {
+            throw new NotImplementedException();
+        }
+*/
+        [HttpPost]
+        public ViewResult ViewDataSubmit()
+        {
+            ViewData["name"] = Request.Form["name"];
+            ViewData["address"] = Request.Form["address"];
+            ViewData["class"] = Request.Form["class"];
+            ViewData["year"] = Request.Form["year"];
+
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult ViewBagSubmit()
+        {
+            ViewBag.name = Request.Form["name"];
+            ViewBag.address = Request.Form["address"];
+            ViewBag.sclass = Request.Form["sclass"];
+            ViewBag.year = Request.Form["year"];
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult TempDataRedirect()
+        {
+            TempData["name"] = Request.Form["name"].ToString();
+            TempData["address"] = Request.Form["address"].ToString();
+            TempData["class"] = Request.Form["class"].ToString();
+            TempData["year"] = Request.Form["year"].ToString();
+
+            return RedirectToAction("TempDataSubmit");
+        }
+
+        public ViewResult TempDataSubmit()
+        {
+            var model = TempData["name"];
+            return View();
+        }
+
+    }
+}
+
